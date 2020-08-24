da9d8103b20d364f9d278192f043f5e15e59b9c4
minpet <minpet@build-machine.local>
2020-08-25 00:04:13 +0200

    fix prod env typo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
+const host = 'chef-server.local';
+
 export const environment = {
   production: true,
-
-  const host = 'chef-server.local';
 
   restCookbooksListBaseURL: `http://${host}:8889/cookbooks`,
   restNodesListBaseURL: `http://${host}:8889/nodes`
