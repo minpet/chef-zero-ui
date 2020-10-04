3f3406f667feb4aaa2678424617407bccb59ddf7
minpet <minpet@build-machine.local>
2020-10-05 01:35:44 +0200

    use https for the backend server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ const host = 'chef-server.local';
 export const environment = {
   production: true,
 
-  restCookbooksListBaseURL: `http://${host}:8889/cookbooks`,
-  restNodesListBaseURL: `http://${host}:8889/nodes`,
-  restVersionBaseURL: `http://${host}:8889/version`
+  restCookbooksListBaseURL: `https://${host}:8889/cookbooks`,
+  restNodesListBaseURL: `https://${host}:8889/nodes`,
+  restVersionBaseURL: `https://${host}:8889/version`
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,9 +7,9 @@ const host = 'chef-server.local';
 export const environment = {
   production: false,
 
-  restCookbooksListBaseURL: `http://${host}:8889/cookbooks`,
-  restNodesListBaseURL: `http://${host}:8889/nodes`,
-  restVersionBaseURL: `http://${host}:8889/version`
+  restCookbooksListBaseURL: `https://${host}:8889/cookbooks`,
+  restNodesListBaseURL: `https://${host}:8889/nodes`,
+  restVersionBaseURL: `https://${host}:8889/version`
 };
 
 /*
